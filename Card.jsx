97ba8112337db14704aba360408c6f0dc299f75d
Ahmed Belmehnouf <128633214+Untitled-Master@users.noreply.github.com>
2025-02-01 22:18:33 +0100

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,13 @@
+
+function Card() {
+
+    return(
+        <div className="card">
+            <img src="https://i.pinimg.com/736x/f3/1c/12/f31c127bb4c7fced9c399da902fbb7bd.jpg" alt="pfp" />
+            <h2>Card Example</h2>
+            <p>This is a test</p>
+        </div>
+    );
+}
+
+export default Card;
