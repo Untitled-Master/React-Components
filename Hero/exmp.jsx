1dfbb18eb57532779e110da04e4623b3e2560977
Ahmed Belmehnouf <128633214+Untitled-Master@users.noreply.github.com>
2025-02-03 21:56:23 +0100

    Create exmp.jsx

---
+++ Hero/exmp.jsx
@@ -0,0 +1,13 @@
+// Default usage
+<Hero />
+
+// Custom usage
+<Hero 
+    imageSrc="/path/to/image.jpg"
+    title="My Custom Title"
+    subtitle="My Unique Approach"
+    description="Detailed description"
+    tags={['Custom', 'Tags']}
+    ctaText="Learn More"
+    onCtaClick={() => {/* custom handler */}}
+/>
